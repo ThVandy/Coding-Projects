cff70f077f023e74e015a47baee2f644ce4a1efc
Skylor VanderLinden <thvandy@gmail.com>
2023-06-15 02:21:27 -0400

    Struggling
    I seem to not know anything about programming.

---
+++ To Do List App/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args, int X)
+    {
+        int X = 1;
+        Console.WriteLine("Hello, World!");
+       
+
+        Console.WriteLine(X);
+    }
+}
