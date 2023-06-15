f1b7e39e77d1c3208a3f06c4836453506ae67940
Skylor VanderLinden <thvandy@gmail.com>
2023-06-15 05:19:45 -0400

    Making progress
    I am now able to set my bottom text box to what my top text box is.

---
+++ To Do App/form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_App
+{
+    public partial class form1 : Form
+    {
+        private string newTask;
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            newTask = newTaskBox.Text;
+            MessageBox.Show("You have added a task!");
+            tasksBox.Text = newTask;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
